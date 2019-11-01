0928191076d790c2476a81ceb7bfd9e704b26ad7
Herna Michel <labellehernamichel@yahoo.fr>
2019-10-31 22:28:59 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,10 +3,40 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      const band = {
+        id: Math.random(),
+        name: action.name }
 
-      return { ...state, bands: [...state.bands, action.name] }
+      return { bands: state.bands.concat(band) }
 
+    case 'DELETE_BAND':
+
+      return { bands: state.bands.filter(band => band.id !== action.id) };
+    
     default:
       return state;
   }
-};
+}
+
+
+
+// export default function manageBand(state = {
+//   bands: []
+// }, action) {
+//   console.log(action)
+//   switch (action.type) {
+//     case 'ADD_BAND':
+//       const band = {
+//         id: Math.random()*10000000000000000,
+//         name: action.name
+//       }
+//       return { ...state, bands: [...state.bands, band] }
+
+//     case 'DELETE_BAND':
+//       const bands = state.bands.filter(band => band.id !== action.id)
+//         return { bands };
+
+//     default:
+//       return state;
+//   }
+// };
